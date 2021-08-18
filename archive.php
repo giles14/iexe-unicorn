9e93f46c4cf9d3331991cfbc257a2f2b405b279a
Alexandro Giles <giles.14@gmail.com>
2021-08-18 17:35:57 -0500

    front-end changes to blog section, both archive.php and blog.css were changed

--- archive.php
+++ archive.php
@@ -1,10 +1,10 @@
 <?php get_header( ); ?>
-<section id="seleccion-categoria">
+<section id="seleccion-categoria" class="buffer-blog">
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h1>Categorías</h1>
-                <p>Elige el programa en el que deseas inscribirte para ver todo el proceso</p>
+                <h1 class="titulo">Categorías</h1>
+                <h2 class="description">Elige el programa en el que deseas inscribirte para ver todo el proceso</h2>
             </div>
             <div class="col-md-2">
             </div>
@@ -38,13 +38,52 @@
     </div>
 </section>
 
-    <section id="ultimas-noticias">
     
-    </section>
     <section id="ultimas-entradas">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 class="titulo">Entradas de Blog</h1>
+                    <h2 class="description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</h2>
+                </div>  
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="bloque-entrada-blog">
+                        <img class="entrada-destacada img-fluid" src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/blogMiniatura.webp" alt="Alternativo de la imagen desplegada">
+                        <div class="bloque-flecha"></div>
+                        <div class="bloque-entrada">
+                            <span class="meta fecha">15 Junio, 2021</span>
+                            <span class="meta categoria">Blog</span>
+                            <h2 class="titulo">Encabezado principal de la noticia hasta tres renglones de descripción</h2>
+                            <p>Quiere la boca exhausta vid, kiwi, piña y fugaz jamón. Fabio me exige, sin tapujosm que añada cerveza al whisky. Jovencillo emponzoñado de whisky, ¡que figurota exhibes!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
 
+                </div>
+                <div class="col-md-4">
+
+                </div>
+            </div>
+        </div>
+        
+
+    </section>
+    <section id="ultimas-noticias">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 class="titulo">Últimas noticias..</h1>
+                    <h2 class="description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</h2>
+                </div>
+                
+            </div>
+        </div>
+        
     </section>
     <section id="noticia-destacada">
     </section>
-    <?php get_template_part( 'template-parts/vinculos'); ?>
+    <?php // get_template_part( 'template-parts/vinculos'); ?>
     <?php get_footer( ); ?>
