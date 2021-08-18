341f09cfe5b0c840d323ade39a71416636d01058
Alexandro Giles <giles.14@gmail.com>
2021-08-18 13:40:26 -0500

    adding working directory of assets without images or videos

---
+++ archive.php
@@ -0,0 +1,50 @@
+<?php get_header( ); ?>
+<section id="seleccion-categoria">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Categorías</h1>
+                <p>Elige el programa en el que deseas inscribirte para ver todo el proceso</p>
+            </div>
+            <div class="col-md-2">
+            </div>
+
+            <div class="col-md-4">
+                <div class="buscar">Buscar</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="categoria-img seguridad">
+                    <h2>Seguridad Pública</h2>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="categoria-img politicas">
+                    <h2>Políticas Públicas</h2>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="categoria-img liderazgo">
+                    <h2>Liderazgo</h2>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="categoria-img internacionales">
+                    <h2>Internacionales</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <section id="ultimas-noticias">
+    
+    </section>
+    <section id="ultimas-entradas">
+
+    </section>
+    <section id="noticia-destacada">
+    </section>
+    <?php get_template_part( 'template-parts/vinculos'); ?>
+    <?php get_footer( ); ?>
---
+++ assets/js/academico.js
@@ -0,0 +1,116 @@
+var $ = jQuery;
+$(document).ready(function(){
+    $("section#navegacion a").click(function(){
+        $("section#navegacion a").removeClass('active');
+        $(this).addClass('active');
+
+    });
+    $('#informacion-programa').carousel({
+        interval: false,
+     });
+     $('#seleccionperiodo').change(function(){
+        $('.bloque-materia').closest('.col-md-4').hide({easing : 'linear'});
+        
+        $('.' + $(this).val()).closest('.col-md-4').show('20');
+    });
+
+    function changeDiv(value) {
+        $('.bloque-materia').closest('.col-md-4').hide({ easing: 'linear' });
+    
+        $('.' + value).closest('.col-md-4').show('20');
+    }
+    
+    var value = $('#seleccionperiodo').val();
+    changeDiv(value);
+
+
+    // $('section#header').click(function(){
+
+    //     $("section#header").css({"background" : "url(" + imagese() + ")", "background-position" : 'center'});
+
+    // });
+
+    // function imagese(){
+    //     var numero = Math.floor(Math.random() * (4 - 1)) + 1
+    //     var urlT = 'https://iexe.xyz/wp-content/themes/iexe-unicorn/assets/img/licenciatura-bg-' +numero+'.webp' 
+    //     return urlT;
+
+    // }
+    
+
+
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var opacity;
+    var current = 1;
+    var steps = $("fieldset").length;
+
+    setProgressBar(current);
+
+    $(".next").click(function(){
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //Add Class Active
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+    step: function(now) {
+    // for making fielset appear animation
+    opacity = 1 - now;
+
+    current_fs.css({
+    'display': 'none',
+    'position': 'relative'
+    });
+    next_fs.css({'opacity': opacity});
+    },
+    duration: 500
+    });
+    setProgressBar(++current);
+    });
+
+    $(".previous").click(function(){
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //Remove class active
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+    step: function(now) {
+    // for making fielset appear animation
+    opacity = 1 - now;
+
+    current_fs.css({
+    'display': 'none',
+    'position': 'relative'
+    });
+    previous_fs.css({'opacity': opacity});
+    },
+    duration: 500
+    });
+    setProgressBar(--current);
+    });
+
+    function setProgressBar(curStep){
+    var percent = parseFloat(100 / steps) * curStep;
+    percent = percent.toFixed();
+    $(".progress-bar")
+    .css("width",percent+"%")
+    }
+
+    $(".submit").click(function(){
+    return false;
+    });
+
+
+});
