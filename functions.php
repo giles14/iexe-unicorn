5d2b5541f03de54fcacb803a6fd0c2155dc794bc
Alexandro Giles <giles.14@gmail.com>
2021-08-25 18:27:49 -0500

    Adding support for sharing buttons not yet finish

--- functions.php
+++ functions.php
@@ -155,3 +155,56 @@ function iexe_unicorn_unset_url_field($fields){
        unset($fields['url']);
        return $fields;
 }
+
+// Function to handle the thumbnail request
+function get_the_post_thumbnail_src($img)
+{
+  return (preg_match('~\bsrc="([^"]++)"~', $img, $matches)) ? $matches[1] : '';
+}
+function wpvkp_social_buttons($content) {
+    global $post;
+    if(is_singular() || is_home()){
+    
+        // Get current page URL 
+        $sb_url = urlencode(get_permalink());
+ 
+        // Get current page title
+        $sb_title = str_replace( ' ', '%20', get_the_title());
+        
+        // Get Post Thumbnail for pinterest
+        $sb_thumb = get_the_post_thumbnail_src(get_the_post_thumbnail());
+ 
+        // Construct sharing URL without using any script
+        $twitterURL = 'https://twitter.com/intent/tweet?text='.$sb_title.'&amp;url='.$sb_url;
+        $facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$sb_url;
+        $bufferURL = 'https://bufferapp.com/add?url='.$sb_url.'&amp;text='.$sb_title;
+        $whatsappURL = 'whatsapp://send?text='.$sb_title . ' ' . $sb_url;
+        $linkedInURL = 'https://www.linkedin.com/shareArticle?mini=true&url='.$sb_url.'&amp;title='.$sb_title;
+
+       if(!empty($sb_thumb)) {
+            $pinterestURL = 'https://pinterest.com/pin/create/button/?url='.$sb_url.'&amp;media='.$sb_thumb[0].'&amp;description='.$sb_title;
+        }
+        else {
+            $pinterestURL = 'https://pinterest.com/pin/create/button/?url='.$sb_url.'&amp;description='.$sb_title;
+        }
+ 
+        // Based on popular demand added Pinterest too
+        $pinterestURL = 'https://pinterest.com/pin/create/button/?url='.$sb_url.'&amp;media='.$sb_thumb[0].'&amp;description='.$sb_title;
+ 
+        // Add sharing button at the end of page/page content
+        $content .= '<div class="social-box"><div class="social-btn">';
+        $content .= '<a class="col-1 sbtn s-twitter" href="'. $twitterURL .'" target="_blank" rel="nofollow"><span><i class="uil uil-twitter"></i></span></a>';
+        $content .= '<a class="col-1 sbtn s-facebook" href="'.$facebookURL.'" target="_blank" rel="nofollow"><span><i class="uil uil-facebook-f"></i></span></a>';
+        $content .= '<a class="col-2 sbtn s-whatsapp" href="'.$whatsappURL.'" target="_blank" rel="nofollow"><span><i class="uil uil-whatsapp"></i></span></a>';
+        $content .= '<a class="col-2 sbtn s-pinterest" href="'.$pinterestURL.'" data-pin-custom="true" target="_blank" rel="nofollow"><span>Pin It Pinter</span></a>';
+        $content .= '<a class="col-2 sbtn s-linkedin" href="'.$linkedInURL.'" target="_blank" rel="nofollow"><span><i class="uil uil-linkedin"></i></span></a>';
+        $content .= '</div></div>';
+        
+        return $content;
+    }else{
+        // if not a post/page then don't include sharing button
+        return $content;
+    }
+};
+
+add_shortcode('social','wpvkp_social_buttons');
