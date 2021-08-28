66d1f1f4864feea3a83fed9427e4a531f87af00d
Alexandro Giles <giles.14@gmail.com>
2021-08-28 16:11:50 -0500

    minor changes to comunidad.php template in order to make it mobile-friendly

--- template-parts/comunidad.php
+++ template-parts/comunidad.php
@@ -15,14 +15,14 @@
                 </div>
             </div>
             <div class="row galeria">
-                <div style="height: 372px;" class="col-4"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/gde-1.png" class="img-fluid" alt=""></div>
+                <div class="col-4"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/gde-1.png" class="img-fluid foto-grande" alt=""></div>
                 <div style="height: 180px;" class="col-4">
                     <div class="bloque-foto d-table" style="margin: 0 6px 6px 0;"><div class="mensaje cyan-primario azul-primario-color d-table-cell align-middle">+9 países</div></div>
                     <div class="bloque-foto d-table" style="margin: 0px 0 6px 6px;"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/c-1.png" class="img-fluid" alt=""></div>
                     <div class="bloque-foto d-table" style="margin: 6px 6px 0 0;"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/c-2.png" class="img-fluid" alt=""></div>
                     <div class="bloque-foto d-table" style="margin: 6px 0 0 6px;"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/c-3.png" class="img-fluid" alt=""></div> 
                 </div>
-                <div style="height: 372px;" class="col-4"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/gde-2.png" class="img-fluid" alt=""></div>
+                <div class="col-4"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/gde-2.png" class="img-fluid foto-grande" alt=""></div>
             </div>
             <div class="row galeria">
                 <div style="height: 180px;" class="col-4">
@@ -31,7 +31,7 @@
                     <div class="bloque-foto d-table" style="margin: 6px 6px 0 0;"><div class="mensaje cyan-primario blanco-color azul-primario d-table-cell align-middle">20 <br> años</div></div>
                     <div class="bloque-foto d-table" style="margin: 6px 0 0 6px;"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/c-1.png" class="img-fluid" alt=""></div> 
                 </div>
-                <div style="height: 372px;" class="col-4"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/gde-3.png" class="img-fluid" alt=""></div>
+                <div class="col-4"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/gde-3.png" class="img-fluid foto-grande" alt=""></div>
                 <div style="height: 180px;" class="col-4">
                     <div class="bloque-foto d-table" style="margin: 0 6px 6px 0;"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/comunidad/c-2.png" class="img-fluid" alt=""></div>
                     <div class="bloque-foto d-table" style="margin: 0px 0 6px 6px;"><div class="mensaje fuente-pequeña azul-obscuro blanco-color d-table-cell align-middle">#comunidad IEXE</div></div>
