f6a51b932815daccf0c602e736dcfdc2a1e2f491
Alexandro <giles.14@gmail.com>
2021-08-29 15:43:21 -0500

    Adding programas.php and related style declarations

---
+++ programas.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Template Name: Programas
+*/
+?>
+<?php get_header( ); ?>
+<section id="principal">
+    <div class="imagen-principal-completa">
+
+    </div>
+</section>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="titulo">Oferta Educativa</h1>
+            <h2 class="description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</h2>
+        </div>
+    </div>
+</div>
+<section id="licenciaturas">
+    <div class="container">
+        <div class="row contenedor-programa">
+            <div class="col-md-6">
+                <h1 class="nombre-programa"><i class="cyan uil uil-angle-right"></i>Licenciaturas</h1>
+                <ul class="licenciaturas">
+                    <li><a href="#">Administración de Empresas</a> </li>
+                    <li><a href="#">Ciencias Políticas y Administración Pública</a> </li>
+                    <li><a href="#">Seguridad Pública</a> </li>
+                    <li><a href="#">Derecho</a> </li>
+                </ul>
+            </div>
+        </div>
+    </div>  
+</section>
+<section id="maestrias">
+    <div class="container">
+        <div class="row contenedor-programa">
+            <div class="col-md-6 offset-md-6">
+                <h1 class="nombre-programa"><i class="cyan uil uil-angle-right"></i>Maestrías</h1>
+                <ul class="maestrias">
+                    <li><a href="#">Administración de Negocios</a> </li>
+                    <li><a href="#">Ingeniería en Tecnologías de la Información</a> </li>
+                    <li><a href="#">Administración y Políticas Públicas</a></li>
+                    <li><a href="#">Evaluación de Políticas Públicas</a></li>
+                    <li><a href="#">Finanzas Públicas</a></li>
+                    <li><a href="#">Seguridad Pública y Políticas Públicas</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>  
+</section>
+<section id="doctorado">
+    <div class="container">
+        <div class="row contenedor-programa">
+            <div class="col-md-6">
+                <h1 class="nombre-programa">Doctorado</h1>
+                <ul class="doctorados">
+                    <li><a href="#">Políticas Públicas</a> </li>
+                </ul>
+            </div>  
+        </div>
+    </div>
+</section>
+<section id="masters">
+    <div class="container">
+        <div class="row contenedor-programa">
+            <div class="col-md-6 offset-md-6">
+                <h1 class="nombre-programa">Másters</h1>
+                <ul class="masters">
+                    <li><a href="#">Innovación y Gestión Educativa</a> </li>
+                    <li><a href="#">Ciencia de Datos Aplicada</a> </li>
+                    <li><a href="#">Inteligencia Artificial Aplicada</a> </li>
+                </ul>
+            </div>  
+        </div>
+    </div>
+</section>
+<section id="diplomados">
+    <div class="container">
+        <div class="row contenedor-programa">
+            <div class="col-md-6">
+                <h1 class="nombre-programa"><i class="cyan uil uil-angle-right"></i>Diplomados</h1>
+                <ul class="displomados">
+                    <li><a href="#">Disciplina Financiera y Fiscalización</a> </li>
+                    <li><a href="#">Evaluación de Políticas Públicas</a> </li>
+                    <li><a href="#">Habilidades Gerenciales y Liderazgo</a> </li>
+                    <li><a href="#">Análisis y Diseño de Políticas Públicas</a> </li>
+                    <li><a href="#">Presupuesto basado en Resultados (PbR)</a></li>
+                    <li><a href="#">Alta Dirección en la Función Pública</a></li>
+                         
+                </ul>
+            </div>
+        </div>
+    </div>  
+</section>
+<section id="capacitaciones">
+    <div class="container">
+        <div class="row contenedor-programa">
+            <div class="col-md-6 offset-md-6">
+                <h1 class="nombre-programa">Capacitaciones</h1>
+                <ul class="capacitaciones">
+                    <li><a href="#">Administración de Empresas</a> </li>
+                    <li><a href="#">Ciencias Políticas y Administración Pública Pública</a> </li>
+                    <li><a href="#">Seguridad Pública</a> </li>
+                    <li><a href="#">Derecho</a> </li>
+                </ul>
+            </div>  
+        </div>
+    </div>
+</section>
+<?php get_template_part( 'template-parts/interes'); ?>
+<?php get_template_part( 'template-parts/comunidad'); ?>
+<?php get_template_part( 'template-parts/vinculos'); ?>
+<?php get_footer( ); ?>
---
+++ template-parts/interes.php
@@ -0,0 +1,46 @@
+<section id="interes">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="titulo">¿Te interesa algún programa?</h1>
+                </div>
+            </div>
+            <div class="row buffer-60">
+                <div class="col-md-6">
+                        <img class="img-fluid escudo" src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/escudo.webp" alt="">
+                </div>
+                <div class="fondo-x">
+                    <img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/img/x.png" alt="">
+                </div>
+                <div class="col-md-5">
+                    <p class="interes-programa">Someday is not a fucking day of the week. Whatever can be captured in words can be conquered with understanding. Defy fucking convention. If you don’t test your will against the dissent of the world, it can never grow stronger.</p>
+                    <form id="contacto">
+                        <div class="form-group">
+                          <label for="nombre">Nombre completo*</label>
+                          <input type="nombre" placeholder="Ingresa aquí tu nombre" class="form-control" id="nombre" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group">
+                            <label for="correo">Correo electrónico*</label>
+                            <input type="email" placeholder="Ingresa aquí tu correo electrónico" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group">
+                            <label for="telefono">Teléfono o celular*</label>
+                            <input type="tel" placeholder="Ingresa aquí tu número telefónico" class="form-control" id="telefono" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group">
+                            <label for="interes">Programa académico de interés</label>
+                            <select class="form-control" id="interes" name="select">
+                                <option value="value1" selected>Seleccionar programa</option>
+                                <option value="value2">Value 2</option>
+                                <option value="value3">Value 3</option>
+                            </select>
+                        </div>
+                        <div class="col-12 text-center">
+                            <button type="submit" class="btn btn-primario mt-3">Enviar registro</button>
+                        </div>
+                      </form>
+
+                </div>
+            </div>
+        </div>
+    </section>
